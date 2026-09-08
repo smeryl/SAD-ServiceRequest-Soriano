5d119f9613e788b4e464a3a6578b80f272f7d9d4
smeryl <msoriano58@adssu.edu.ph>
2026-09-07 21:07:59 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // js/supabase.js
 
-const SUPABASE_URL = 'https://your-project-id.supabase.co';
-const SUPABASE_ANON_KEY = 'your-anon-public-key';
+const SUPABASE_URL = 'https://ntnfwpvdiiimzehirdous.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bmZ3cHZkaWltemVoaXJkb3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4MDQyMjAsImV4cCI6MjEwNDM4MDIyMH0.St8y6K8jG7zatecnXxza5S6bhTN_1RTDXnNXeHUHtO4';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
